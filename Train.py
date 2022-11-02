1448593a378c0e1f27beb006b069693acad94f54
Ved Thakur <thakur.6@iitj.ac.in>
2022-11-02 16:38:51 +0530

    Check for file

--- Train.py
+++ Train.py
@@ -222,10 +222,11 @@ if __name__ == '__main__':
 
     print("#" * 20, "Start Training", "#" * 20)
     if load_model:
-        load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+        if os.path.isdir('my_checkpoint.pth.tar') == True:
+            load_checkpoint(torch.load("my_checkpoint.pth.tar"))
 
     for epoch in range(1, opt.epoch):
-        if epoch == 2:
+        if epoch == 1:
            checkpoint = {'state_dict' : model.state_dict(), 'optimizer': optimizer.state_dict()}
            save_checkpoint(checkpoint)
             
